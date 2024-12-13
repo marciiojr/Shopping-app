3e1f360f1551717477048e97d74336f743d5827d
marciiojr <marciojr_@live.com>
2024-12-13 12:10:10 -0300

    adc arq

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            amarelo: '#E6C744',
+            cinza: '#C2C8DA',
+            cinzaclaro: '#F4F6F5',
+            fontcinza: '#7F7F7F'
+        }
+      },
+    },
+    plugins: [],
+  }
